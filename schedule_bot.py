af70bd60ba59f9d27ede3a4eddef8df41fb4b19b
Zach Adams <zachtundra57@gmail.com>
2026-07-02 16:50:17 -0500

    Initial file

---
+++ schedule_bot.py
@@ -0,0 +1,13 @@
+import discord
+
+class ScheduleClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user}')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = ScheduleClient(intents=intents)
