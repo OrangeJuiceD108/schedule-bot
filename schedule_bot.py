3971dfbf396eb4377b3c43ea85169bc8cd169cd2
Zach Adams <zachtundra57@gmail.com>
2026-07-07 02:12:29 -0500

    Complete?

--- schedule_bot.py
+++ schedule_bot.py
@@ -45,13 +45,39 @@ async def on_scheduled_event_update(before, after):
         role = after.description.split(" ")[1]
     database.add_event(after.id, after.start_time, role, after.guild.id, after.name)
 
+class RemindFlags(commands.FlagConverter, prefix='-', delimiter = ' '):
+    w: int = 0
+    d: int = 0
+    h: int = 0
+    m: int = 0
+
 @bot.command()
-async def remind(ctx, args):
-    await ctx.send("nah")
+async def remind(ctx, *, flags: RemindFlags):
+    delta = datetime.timedelta(days=(flags.d + flags.w * 7), hours=flags.h, minutes=flags.m)
+    await ctx.send(f"Reminder set! I\'ll remind you {delta} before every event in this server!")
+    if flags.m % 15 != 0:
+        await ctx.send('''WARNING: I check the time every 15 minutes, 
+        so while I can *accept* timedelta values that are not evenly divisible by 15, 
+        I will still only remind you the next time I check the time, 
+        rather than the time you're asking for. Sorry!''')
 
 @bot.command(name='help')
-async def custom_help(ctx, args=None):
-    await ctx.send('nah')
+async def custom_help(ctx):
+    await ctx.send('''
+    To use Heroin, type \'!remind\', followed by your desired flags.
+    
+    I\'ve got 4 flags:
+    > \'-w\' => How many weeks before each event you want to be reminded
+    > \'-d\' => How many days 
+    > \'-h\' => How many hours
+    > \'-m\' => How many minutes
+    
+    Here are some examples:
+    > `!remind -w 1 -d 5 -h 2 -m 15`
+    > `!remind -d 2 -h 6`
+    
+    NOTE: Reminders are checked every 15 minutes. Minute values indivisible by 15 are allowed, but no different from minute values rounded up to the next multiple of 15.
+    ''')
 
 @tasks.loop(minutes=15)
 async def check_reminder():
